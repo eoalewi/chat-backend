0d367e8eff234e6d8d7bb95ac06e06765ccc6203
oealewi <alewitosino208@gmail.com>
2025-08-21 08:04:44 +0100

    Remove dist from repo

--- dist/socket/authSocket.d.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from "socket.io";
-export type AuthedSocket = Socket & {
-    user?: {
-        id: number;
-        email: string;
-    };
-};
-export declare function socketAuthMiddleware(socket: AuthedSocket, next: (err?: Error) => void): void;
-//# sourceMappingURL=authSocket.d.ts.map
--- dist/utils/token.d.ts
+++
@@ -1,4 +0,0 @@
-import jwt from "jsonwebtoken";
-export declare const generateToken: (payload: object, expiresIn?: string | number) => string;
-export declare const verifyToken: (token: string) => string | jwt.JwtPayload;
-//# sourceMappingURL=token.d.ts.map
